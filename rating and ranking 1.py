0db80bbda2fa036f9c82a065bd4030472b18e7f0
John Hamilton <94325601+johngilberthamilton@users.noreply.github.com>
2023-05-08 18:47:43 -0400

    final results that went into presentation

--- rating and ranking 1.py
+++ rating and ranking 1.py
@@ -23,6 +23,7 @@ wg = pd.read_csv('/2022NFLseason.csv')
 wg = wg.rename(columns={'Unnamed: 5': 'homeaway','Winner/tie':'winner','Loser/tie':'loser'})
 wg = wg.drop('Unnamed: 7', axis = 1)
 
+
 for lab, row in wg.iterrows():
     if wg.loc[lab,'homeaway'] == '@':
         wg.loc[lab,'homeaway'] = -2
@@ -37,9 +38,11 @@ wg['diff'] = wg.PtsW - wg.PtsL
 wg_reg_season = wg[0:271]
 wg_reg_season['Week'] = wg_reg_season['Week'].astype(np.int64)
 
-p_matrix = wg_reg_season.pivot_table(index = 'winner', columns = 'loser', values = 'diff').fillna(0)  - wg_reg_season.pivot_table(index = 'winner', columns = 'loser', values = 'diff').fillna(0).T
 
-P = wg_reg_season.pivot_table(index = 'winner', columns = 'loser', values = 'diff', aggfunc = 'count').fillna(0) + wg_reg_season.pivot_table(index = 'winner', columns = 'loser', values = 'diff', aggfunc = 'count').fillna(0).T 
+# Massey Method
+p_matrix = wg_reg_season.pivot_table(index = 'winner', columns = 'loser', values = 'diff', aggfunc='sum').fillna(0)  -    wg_reg_season.pivot_table(index = 'winner', columns = 'loser', values = 'diff', aggfunc = 'sum').fillna(0).T
+
+P =        wg_reg_season.pivot_table(index = 'winner', columns = 'loser', values = 'diff', aggfunc = 'count').fillna(0) + wg_reg_season.pivot_table(index = 'winner', columns = 'loser', values = 'diff', aggfunc = 'count').fillna(0).T 
 num_teams = len(pd.unique(wg_reg_season[['winner','loser']].to_numpy().flatten()))
 T = pd.DataFrame(np.identity(n = num_teams), index = P.index, columns = P.columns)
 for i, row in T.iterrows():
@@ -48,7 +51,6 @@ for i, row in T.iterrows():
     
 M = T - P
     
-# let's solve for the basic massey method - Mr = p
 
 # worrying about full rank isn't necessary for this year because we're missing a game
 # M_full_rank = M.copy()
@@ -78,10 +80,10 @@ for lab, row in ratings_df.iterrows():
     ratings_df.loc[lab,'losses'] = len(losers_df) - tiesl
     ratings_df.loc[lab,'ties'] = tiesw + tiesl
 
-ratings_df['massey'] = r.tolist()
+
 
 # massey offense and defense
-f_matrix =  wg_reg_season.pivot_table(index = 'winner', columns = 'loser', values = 'PtsW').fillna(0)  + wg_reg_season.pivot_table(index = 'winner', columns = 'loser', values = 'PtsL').fillna(0).T
+f_matrix =  wg_reg_season.pivot_table(index = 'winner', columns = 'loser', values = 'PtsW', aggfunc='sum').fillna(0)  + wg_reg_season.pivot_table(index = 'winner', columns = 'loser', values = 'PtsL', aggfunc = 'sum').fillna(0).T
 a_matrix = f_matrix - p_matrix
 f = f_matrix.sum(axis = 1).values
 a = a_matrix.sum(axis = 1).values
@@ -89,7 +91,9 @@ a = a_matrix.sum(axis = 1).values
 d = linalg.lstsq(T + P,T@r - f)[0]
 
 o = r - d
-
+ratings_df['pf'] = f
+ratings_df['pa'] = a
+ratings_df['massey'] = r.tolist()
 ratings_df['massey_o'] = o.tolist()
 ratings_df['massey_d'] = d.tolist()
 
@@ -142,7 +146,7 @@ for week in range(1,19):
     ratings_elo_time[week] = ratings_elo_time_series
 ratings_df['elo'] = ratings_elo_time_series
 
-ratings_elo_time.to_csv('elo1.csv')
+ratings_elo_time.to_csv('C:/Users/hamilj37/OneDrive - Pfizer/Documents/ranking/elo1.csv')
 
 # notes: comparing to fivethirtyeight
 # fivethirtyeight uses priors
@@ -173,3 +177,60 @@ for week in range(1,19):
         ratings_elo_time_series[j] = rj_old + k*(Sji - muji)
     ratings_elo_time_points[week] = ratings_elo_time_series
 ratings_df['elo_points'] = ratings_elo_time_series
+
+# markov method
+
+#1. losses
+
+V1 = wg_reg_season.pivot_table(index = 'winner', columns = 'loser', values = 'diff', aggfunc = 'count').fillna(0)
+# fixing ties
+V1.loc['Houston Texans', 'Indianapolis Colts'] = .5
+V1.loc['Indianapolis Colts', 'Houston Texans'] = 1.5
+V1.loc['New York Giants','Washington Commanders'] = .5
+V1.loc['Washington Commanders', 'New York Giants'] = 1.5
+V1.loc['Cincinnati Bengals', 'Buffalo Bills'] = .5
+V1.loc['Buffalo Bills', 'Cincinnati Bengals'] = .5
+# V1.loc['Houston Texans', 'Indianapolis Colts'] = 0
+# V1.loc['Indianapolis Colts', 'Houston Texans'] = 0
+# V1.loc['New York Giants','Washington Commanders'] = 0
+# V1.loc['Washington Commanders', 'New York Giants'] = 0
+# V1.loc['Cincinnati Bengals', 'Buffalo Bills'] = 0
+# V1.loc['Buffalo Bills', 'Cincinnati Bengals'] = 0
+V2 = V1.T.copy()
+# V3 = V2.copy()
+# list = [1]
+# for i in range(31):
+#     list.append(1)
+# V3.loc['dummy team'] = list
+# V3['dummy team'] = 1
+
+N1= V2.div(V2.sum(axis = 1), axis = 0)
+eigen_values, eigen_vectors = eig(N1.T)
+df_eigen = pd.DataFrame(eigen_vectors, columns = eigen_values, index = V2.index)
+r = df_eigen.iloc[:,0]/df_eigen.iloc[:,0].sum()
+ratings_df['markov_losses'] = np.real(r)
+
+
+
+# offense-defense method
+d0 = np.array([1 for i in range(32)])
+
+
+A = a_matrix.copy() 
+# loop through a game matrix to find averages instead of totals for multi-game matchups
+for i, row in A.iterrows():
+    for j in list(A):
+        if M.loc[i,j] == -2:
+            A.loc[i,j] = A.loc[i,j] / 2
+            
+o0 = A.T @ np.reciprocal(d0)
+
+
+o1 = o0
+d1 = d0
+for i in range(50):
+    d1 = A @ np.reciprocal(o1)
+    o1 = A.T @np.reciprocal(d1)
+ratings_df['od_o'] = o1
+ratings_df['od_d'] = d1
+ratings_df['od_r'] = o1/d1
